fda855b5a736ed875bfee2b766f84b2a11ce5eff
Stianjp <114061998+Stianjp@users.noreply.github.com>
2024-02-16 15:55:25 +0100

    Ferdig med enhetstester for bankcontroller

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -80,24 +80,152 @@ public class EnhetstestBankController {
         assertNull(resultat);
     }
     @Test
-    public void test_hentKonti(){ // ENHETSTEST FOR hentKonti
+    public void test_hentSaldi_LoggetInn(){ // ENHETSTEST FOR hentSaldi
         //arrange
-        List<Konto> konti = new ArrayList<>();
-        Konto konti1 = new Konto("12345654", "1234565432", 420, "Lønnskonto", "EUR", null);
-        Konto konti2 = new Konto("12665654", "1234565532", 720, "Sparekonto", "NOK", null);
+        List<Konto> saldi = new ArrayList<>();
 
-        konti.add(konti1);
-        konti.add(konti2);
+        Konto saldi1 = new Konto("12345654", "1234565432", 420, "Lønnskonto", "EUR", null);
+        Konto saldi2 = new Konto("12665654", "1234565532", 720, "Sparekonto", "NOK", null);
+
+        //Legger de fiktive transaksjonene i listen
+        saldi.add(saldi1);
+        saldi.add(saldi2);
 
         when(sjekk.loggetInn()).thenReturn("345654345654");
-        when(repository.hentKonti(anyString())).thenReturn(konti);
+        when(repository.hentSaldi(anyString())).thenReturn(saldi);
 
         // act
-        List<Konto> resultat = bankController.hentKonti();
+        List<Konto> resultat = bankController.hentSaldi();
         // assert
-        assertEquals(konti, resultat, "Feil i hentKonti");
+        assertEquals(saldi, resultat, "Feil i hentSaldi");
     }
 
+    @Test
+    public void test_hentSaldi_IkkeLoggetInn(){
+        //arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+        //act
+        List<Konto> resultat = bankController.hentSaldi();
+
+        //assert
+        assertNull(resultat);
+
+    }
+
+    @Test
+    public void test_hentRegistrerBetaling_loggetInn(){
+        // arrange
+        Transaksjon regBetaling = new Transaksjon();
+        String personnr = "123234234";
+        String forventetResultat = "Betalingen er registrert";
+
+        when(sjekk.loggetInn()).thenReturn(personnr);
+        when(repository.registrerBetaling(any(Transaksjon.class))).thenReturn(forventetResultat);
+
+        //act
+        String resultat = bankController.registrerBetaling(regBetaling);
+
+        // assert
+        assertEquals(forventetResultat, resultat, "Betalingen skulle blitt registrert");
+    }
+
+    @Test
+    public void test_hentRegistrerBetaling_IkkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+        // act
+        String resultat = bankController.registrerBetaling(null);
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void test_hentBetaling_loggetInn(){
+        // arrange
+        List<Transaksjon> hentBet = new ArrayList<>();
+        Transaksjon transaksjon1 = new Transaksjon(1, "234234234", 234.00, "01.01.24", "Test", "1", "2342342344");
+        Transaksjon transaksjon2 = new Transaksjon(2, "2342343244", 420.00, "02.02.24", "en-to-en", "2", "234234234324");
+
+        hentBet.add(transaksjon1);
+        hentBet.add(transaksjon2);
+
+        when(sjekk.loggetInn()).thenReturn("234234234234");
+        when(repository.hentBetalinger(anyString())).thenReturn(hentBet);
+
+        // act
+        List<Transaksjon> resultat = bankController.hentBetalinger();
+        // assert
+        assertEquals(hentBet, resultat, "Feil i hentBetaling");
+    }
+    @Test
+    public void test_hentBetaling_ikkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+        // act
+        List<Transaksjon> resultat = bankController.hentBetalinger();
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void test_utforBetaling_loggetInn(){
+        // arrange
+        List<Transaksjon> utforBet = new ArrayList<>();
+
+        Transaksjon test1 = new Transaksjon(123, "2342343245", 420.01, "01.04.23","hallo", "234234324", "234234234324");
+        Transaksjon test2 = new Transaksjon(321, "5534235645", 720.20, "09.04.21","hei", "24", "65464324");
+        utforBet.add(test1);
+        utforBet.add(test2);
+
+
+        when(sjekk.loggetInn()).thenReturn("234234234324");
+        when(repository.utforBetaling(anyInt())).thenReturn("OK");
+        when(repository.hentBetalinger(anyString())).thenReturn(utforBet);
+
+        // act
+        List<Transaksjon> resultat = bankController.utforBetaling(123);
+
+
+        // assert
+        assertEquals(utforBet, resultat);
+    }
+
+    @Test
+    public void test_utforBetaling_ikkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+        // act
+        List<Transaksjon> resultat = bankController.utforBetaling(0);
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void endreKundeInfo_loggetInn(){
+        // arrange
+        Kunde enKunde = new Kunde();
+        String personrnr = "2342342341";
+        String forventetResultat = "Endring gjennomført";
+
+        when(sjekk.loggetInn()).thenReturn(personrnr);
+        when(repository.endreKundeInfo(any(Kunde.class))).thenReturn(forventetResultat);
+
+        // act
+        String resultat = bankController.endre(enKunde);
+
+        // assert
+        assertEquals(forventetResultat, resultat);
+    }
+
+    @Test
+    public void endreKundeInfo_ikkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+        // act
+        String resultat = bankController.endre(null);
+        // assert
+        assertNull(resultat);
+    }
 
     @Test
     public void hentKundeInfo_loggetInn() {
